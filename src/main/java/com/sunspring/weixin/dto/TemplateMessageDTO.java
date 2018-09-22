51cf5cefbfe28c87156ae4930bcc0af3a43b6e8e
sunhe3 <sunhe3@bonc.com.cn>
2018-09-23 00:02:55 +0800

    微信公众号：发送模板消息

---
+++ src/main/java/com/sunspring/weixin/dto/TemplateMessageDTO.java
@@ -0,0 +1,97 @@
+package com.sunspring.weixin.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+
+/**
+ * 模板消息
+ * @author sunhe
+ */
+@Getter
+@Setter
+public class TemplateMessageDTO {
+
+    /**
+     * 接收者openid
+     */
+    private String touser;
+    /**
+     * 模板ID
+     */
+    private String template_id;
+    /**
+     * 模板跳转链接
+     */
+    private String url;
+    /**
+     * 跳小程序所需数据，不需跳小程序可不用传该数据
+     * 所需跳转到的小程序appid（该小程序appid必须与发模板消息的公众号是绑定关联关系，暂不支持小游戏）
+     */
+    private String appid;
+    /**
+     * 所需跳转到小程序的具体页面路径，支持带参数,（示例index?foo=bar），暂不支持小游戏
+     */
+    private String pagepath;
+    /**
+     * 模板数据
+     */
+    private Map<String, TemplateMessageDataItem> data;
+
+    public TemplateMessageDTO() {
+    }
+
+    public TemplateMessageDTO(String touser, String template_id, String url, String appid, String pagepath, Map <String, TemplateMessageDataItem> data) {
+        this.touser = touser;
+        this.template_id = template_id;
+        this.url = url;
+        this.appid = appid;
+        this.pagepath = pagepath;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "           \"touser\":\"" + touser + "\",\n" +
+                "           \"template_id\":\"" + template_id + "\",\n" +
+                            (StringUtils.isEmpty(url)? "" : "\"url\":\"" + url + "\"\n")+
+                            generateMiniprogram() +
+                "           \"data\":" +generateData() +
+                "}";
+    }
+
+    /**
+     * 处理模板数据
+     * @return data
+     */
+    private String generateData(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (Map.Entry<String, TemplateMessageDataItem> item : data.entrySet()) {
+            sb.append("\"").append(item.getKey()).append("\":");
+            sb.append("{");
+            sb.append("\"value\":\"").append(item.getValue().getValue()).append("\",");
+            sb.append("\"color\":\"").append(item.getValue().getColor()).append("\"");
+            sb.append("},");
+        }
+        return sb.substring(0, sb.length()-1) + "}";
+    }
+    /**
+     * 处理miniprogram
+     * @return data
+     */
+    private String generateMiniprogram(){
+        String miniprogram = "";
+        if(!StringUtils.isEmpty(appid)){
+            miniprogram =" \"miniprogram\":{\n" +
+                        "   \"appid\":\"" + appid + "\",\n" +
+                          (StringUtils.isEmpty(pagepath)? "" : "\"pagepath\":\"" + pagepath + "\"\n")+
+                        " },  ";
+        }
+        return miniprogram;
+    }
+
+}
--- src/main/java/com/sunspring/weixin/utils/WeixinUtil.java
+++ src/main/java/com/sunspring/weixin/utils/WeixinUtil.java
@@ -1,17 +1,28 @@
 package com.sunspring.weixin.utils;
 
+import com.sunspring.weixin.dto.TemplateMessageDTO;
+import com.sunspring.weixin.dto.TemplateMessageDataItem;
 import net.sf.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class WeixinUtil {
 
     public static final String TOKEN = "wx_service_sh";
-    public static final String APP_ID = "wx132b689efeb0f7d7";
-    public static final String APP_SECRET = "c346c42697194237ba92cf203658b58a";
+    private static final String APP_ID = "wx132b689efeb0f7d7";
+    private static final String APP_SECRET = "c346c42697194237ba92cf203658b58a";
+    //创建自定义菜单url
     private static final String CREATE_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+    //删除自定义菜单url
+    private static final String DELETE_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN";
+    //获取accessToke
     private static final String GET_ACCESSTOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+    //发送模板消息
+    private static final String SEND_TEMPLATE_URL = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN";
 
     private static String accessToken;
     private static Long expireTime;
@@ -19,7 +30,7 @@ public class WeixinUtil {
      * 获取Accesstoken凭据
      * @return accessToken
      */
-    public static String getAccessToken() {
+    private static String getAccessToken() {
         //accessToken为空 或 已经失效时进行重新获取
         if(StringUtils.isEmpty(WeixinUtil.accessToken) || System.currentTimeMillis() > WeixinUtil.expireTime){
             JSONObject jsonObject = HttpUtil.get(GET_ACCESSTOKEN_URL.replace("APPID",APP_ID).replace("APPSECRET",APP_SECRET));
@@ -32,7 +43,7 @@ public class WeixinUtil {
         return WeixinUtil.accessToken;
     }
 
-    private static String json = "{\n" +
+    private static String jsonMenu = "{\n" +
             "     \"button\":[\n" +
             "     {    \n" +
             "          \"type\":\"click\",\n" +
@@ -50,13 +61,90 @@ public class WeixinUtil {
             "          \"key\":\"V1001_LOCATION_SELECT\"\n" +
             "      }" +
             " }";
+
+    private static String jsonTemplateMsg = "{\n" +
+            "           \"touser\":\"ohm_z1EjFrY2vTmLbRSK0haIV10w\",\n" +
+            "           \"template_id\":\"8hnejHal2RdZruS5IF3LUgbQbimSBkgugKrw8Vo0h_k\",\n" +
+            "           \"url\":\"https://www.baidu.com\",\n" +
+            "           \"data\":{\n" +
+            "                   \"top\": {\n" +
+            "                       \"value\":\"尊敬的XXX，您好！\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   },\n" +
+            "                   \"deptName\":{\n" +
+            "                       \"value\":\"七里庄营业厅\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   },\n" +
+            "                   \"currentNum\": {\n" +
+            "                       \"value\":\"GH1024\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   },\n" +
+            "                   \"serviceType\": {\n" +
+            "                       \"value\":\"宽带报装\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   },\n" +
+            "                   \"aheadNum\": {\n" +
+            "                       \"value\":\"20人\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   },\n" +
+            "                   \"ewt\": {\n" +
+            "                       \"value\":\"10分钟\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   },\n" +
+            "                   \"bottom\":{\n" +
+            "                       \"value\":\"我们将尽快为您服务，请留意微信平台排号变化通知，中国联通温馨提醒！\",\n" +
+            "                       \"color\":\"#173177\"\n" +
+            "                   }\n" +
+            "           }\n" +
+            "       }";
+
+    /**
+     * 创建自定义菜单
+     * @param jsonParam json数据
+     */
     public static void createMenu(String jsonParam) {
         JSONObject resultJson =  HttpUtil.post(CREATE_MENU_URL.replace("ACCESS_TOKEN",getAccessToken()),jsonParam);
-        System.out.println(">>>>>>>>>>>>>>>>>>>>"+resultJson);
+        System.out.println("createMenu>>>>>>>>>>>>>>>>>>>>"+resultJson);
+    }
+
+    /**
+     * 删除自定义菜单
+     */
+    public static void deleteMenu() {
+        JSONObject resultJson =  HttpUtil.get(DELETE_MENU_URL.replace("ACCESS_TOKEN",getAccessToken()));
+        System.out.println("deleteMenu>>>>>>>>>>>>>>>>>>>>"+resultJson);
+    }
+
+    /**
+     * 发送模板消息
+     */
+    public static void sendTemplateMessage(TemplateMessageDTO messageDTO) {
+        JSONObject resultJson =  HttpUtil.post(SEND_TEMPLATE_URL.replace("ACCESS_TOKEN",getAccessToken()),messageDTO.toString());
+        System.out.println("sendTemplateMessage>>>>>>>>>>>>>>>>>" + resultJson);
     }
 
     public static void main(String[] args) {
-        createMenu(json);
+        Map<String, TemplateMessageDataItem> dataMap = new HashMap<>();
+        TemplateMessageDataItem top = new TemplateMessageDataItem();
+        top.setValue("尊敬的XXX，您好！");
+        top.setColor("#173177");
+        TemplateMessageDataItem bottom = new TemplateMessageDataItem();
+        bottom.setValue("我们将尽快为您服务，请留意微信平台排号变化通知，中国联通温馨提醒！");
+        bottom.setColor("#173177");
+        dataMap.put("top", top);
+        dataMap.put("bottom", bottom);
+        TemplateMessageDTO templateMessageDTO = new TemplateMessageDTO();
+        templateMessageDTO.setTouser("ohm_z1EjFrY2vTmLbRSK0haIV10w");
+        templateMessageDTO.setTemplate_id("8hnejHal2RdZruS5IF3LUgbQbimSBkgugKrw8Vo0h_k");
+        templateMessageDTO.setUrl("https://www.baidu.com");
+
+        templateMessageDTO.setData(dataMap);
+
+
+        sendTemplateMessage(templateMessageDTO);
+
+
+//        createMenu(jsonMenu);
 //        System.out.println(WeixinUtil.accessToken);
 //        System.out.println(getAccessToken());
 //        System.out.println(getAccessToken());
